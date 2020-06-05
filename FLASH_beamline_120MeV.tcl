1b930ce72c98aa93f8d41ac92e5f0c21342899e4
verakorchevnyuk <verakorchevnyuk@hotmail.com>
2020-06-05 18:48:13 +0100

    First try of layout simplification
    1- Goal: simplify layout by removing all elements from lattice (except bends). To leave at least *2 m* for the first arms, *2.7 m* for the second arm;
    2- Separator magnet parameters: yield from tracking software (three beams);
    3- Problems: (a) not all the drifts are removable (ERROR: placet_element_set_attribute: vector of input values must have one element, or as many elements as indexes); (b) placet complains when removing the unused bend (same ERROR as in (a)); (c) the program does not converge.

---
+++ FLASH_beamline_120MeV.tcl
@@ -0,0 +1,29 @@
+set e_initial 0.144 ; # 60 MeV
+set e0 $e_initial
+
+set lquad 0.2
+set lbend 0.8
+set min_length 0.2
+
+# QFO1  : Q1, k1 := 22.18023962  ;
+# QDE2  : Q1, k1 := -22.24163335  ; 
+# QFO3  : Q1, k1 :=  23.39833159  ;
+# QDE4  : Q1, k1 :=  -27.15654042  ;
+# QFO5  : Q1, k1 :=  24.999998  ;
+
+BeamlineNew
+Girder
+Drift -length 2
+
+#Quadrupole -length $lquad -strength [expr -22.24163335 * $lquad * $e0]
+Drift -length 2
+#Quadrupole -length $lquad -strength [expr -22.24163335 * $lquad * $e0]
+#Drift -length 0.4
+
+#Quadrupole -length $lquad -strength [expr 23.39833159 * $lquad * $e0]
+#Drift -length $min_length
+#Quadrupole -length $lquad -strength [expr -27.15654042 * $lquad * $e0]
+#Drift -length $min_length
+#Quadrupole -length $lquad -strength [expr 24.999998 * $lquad * $e0]
+Drift -length 3
+
--- run_optimize_layout.tcl
+++ run_optimize_layout.tcl
@@ -5,9 +5,9 @@ set outdir Results
 exec mkdir -p $outdir
 cd $outdir
 
-set EL 105
+set EL 100
 set EO 120
-set ER 135
+set ER 140
 
 Octave {
     addpath([ pwd '/../disp_mag-octave' ]);
@@ -44,35 +44,55 @@ Octave {
 	
     # MERIT FUNCTION
     function M = merit_function(X)
-    H = 7; # m, target height
+    H = 6; # m, target height
     ALd = 60; # target angle for L beamline
     ARd = 30; # target angle for R beamline
     global B${EL} B${ER} D${EL} D${EO} D${ER} P0 P1 P2
-    DL${EL} = constrain(X(1:2), 0.3, 2); % m
-    DL${EO} = constrain(X(3:4), 0.3, 2); % m
-    DL${ER} = constrain(X(5:6), 0.3, 2); % m
-    BAL = constrain(X(7), 0, 0) % deg
-    BAR = constrain(X(8), 0, 0) % deg
-    beta1 = constrain(X(9), 35, 45) % deg
-    beta2 = constrain(X(10), 0, 90) % deg
-    Bref = 1; #constrain(X(10), 1, 1) % T
-    Bgrad = constrain(X(11), 0, 1.5) # % T/m 43028
+    DL${EL} = constrain(X(1:2), 0, 2); % m
+    DL${EO} = constrain(X(3:4), 0, 2); % m
+    DL${ER} = constrain(X(5:6), 0, 2); % m
+    BAL = 0; #constrain(X(7), 0, 0) % deg
+    BAR = 0; #constrain(X(8), 0, 0) % deg
+    #beta1 = constrain(X(9), 35, 45) % deg
+    #beta2 = constrain(X(10), 0, 90) % deg
+    #Bref = 1; #constrain(X(10), 1, 1) % T
+    #Bgrad = constrain(X(11), 0, 1.5) # % T/m 43028
 
-    [lm, R, Bgrad, Gamma, P0, P1, P2 ] = beam_dynamics_variables( beta1, beta2, Bref, Bgrad, $EO * 1e6, $EL * 1e6, $ER * 1e6, 'n');
-    %[lm, R, Bgrad, Gamma, P0, P1, P2 ] = beam_dynamics_variables( beta1, beta2, Bref, Bgrad, 120 * 1e6, 105 * 1e6, 135 * 1e6, 'n');
+    %[lm, R, Bgrad, Gamma, P0, P1, P2 ] = beam_dynamics_variables( beta1, beta2, Bref, Bgrad, $EO * 1e6, $EL * 1e6, $ER * 1e6, 'n');
+    %[ lm, R, Bgrad, Gamma, P0, P1, P2, theta1, theta2 ] = beam_dynamics_variables( beta1, beta2, Bref, Bgrad, 120 * 1e6, 105 * 1e6, 135 * 1e6, 'n');
+	
+	lm = [ 0.99998694425935408  ; 0.99999093345924772 ; 0.99999333884400476 ] ;
+	#R, Bgrad, Gamma, P0, P1, P2
 
+	#theta1 = 115.87 ;
+	#theta2 = 71.529 ;
+	#theta0 = 90.059 ;
+	#alpha1 = theta1 - theta0 ;
+	#alpha2 = theta0 - theta2 ;
+	#gamma0 = beta ;
+	#gamma1 = alpha1 + beta1 ;
+	#gamma2 = beta2 - alpha2 ;
+	#total_alpha = alpha1 + alpha2 ;
 
-	magnetic_lengths = lm'
+	#magnetic_lengths = lm' ;
     
-    VO = diff(P0,1,1)(end,:);
-    VL = diff(P1,1,1)(end,:);
-    VR = diff(P2,1,1)(end,:);
+    #VO = diff(P0,1,1)(end,:);
+    #VL = diff(P1,1,1)(end,:);
+    #VR = diff(P2,1,1)(end,:);
     
     # SET THE MAIN BENDING ANGLES AND LENGTHS
 
-    AO = atan2d(VO(2), VO(1));
-    AL = atan2d(VL(2), VL(1));
-    AR = atan2d(VR(2), VR(1));
+    #AO = atan2d(VO(2), VO(1));
+    #AL = atan2d(VL(2), VL(1));
+    #AR = atan2d(VR(2), VR(1));
+    
+	AO = -90.058697687489115 ;
+    AL = -115.86546804164304 ;
+	AR = -71.528778310901089 ;
+	
+	P0 = [ 524.66980412173018, -677.60953638218894 ]/1000 ; 
+	P1 = [ 320.59393079582719, -699.89468503429543 ]/1000 ;
+	P2 = [ 666.38034744499737, -617.39155831973233 ]/1000 ;
 
     BAL(2) = (-AO - ALd) + (AL - BAL(1));
     BAR(2) = (-AO + ARd) + (AR - BAR(1));
@@ -104,15 +124,15 @@ Octave {
     xlabel('m');
     ylabel('m');
     daspect ([1 1]);
-    axis([ FO(end,1)-H/2 FO(end,1)+H/2 -(H+1) 0 ]);
+    axis([ FO(end,1)-H/2 FO(end,1)+H/2 -(H+1)-2 0 ]);
     drawnow
     
     M = hypot(FL(end,1) - FO(end,1), FL(end,2) - FO(end,2)) + ...
-        hypot(FR(end,1) - FO(end,1), FR(end,2) - FO(end,2));% + ...
+        hypot(FR(end,1) - FO(end,1), FR(end,2) - FO(end,2)) % + ...
         %0.1 * abs(FO(end,2));
     end
 
-    X = fminsearch(@merit_function, zeros(1,11))
+    X = fminsearch(@merit_function, zeros(1,6))
 
     print -dpng plot_layout.png
 }
